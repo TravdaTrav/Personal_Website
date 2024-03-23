3a3606fbf4cb3f0ac6ed454261de08696a8681cc
Travis Yu Chen <tchen128@jh.edu>
2024-03-22 20:57:10 -0400

    continued working on blog

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+function timeDateStyles() {
+  let timedates = document.querySelectorAll(".time-date");
+  for (let timedate of timedates) {
+    if (timedate.offsetHeight > 40) {
+      timedate.classList.remove("button");
+    } else {
+      timedate.classList.add("button");
+    }
+  }
+}
+
+window.addEventListener("load", timeDateStyles);
+window.addEventListener("resize", timeDateStyles);
